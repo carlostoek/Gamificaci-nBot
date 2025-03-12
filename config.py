1b89d5a8f2feeb075ae986f6d12c6c61adff7ceb
carlostoek <carlostoek@gmail.com>
2025-03-12 10:33:46 -0600

    Update config.py

--- config.py
+++ config.py
@@ -1,9 +1,5 @@
 import os
-from dotenv import load_dotenv
-
-# Cargar variables del archivo .env
-load_dotenv()
 
 TOKEN = os.getenv("BOT_TOKEN")
-CHANNEL_ID = int(os.getenv("CHANNEL_ID", "0"))  # Convertir a entero
+CHANNEL_ID = int(os.getenv("CHANNEL_ID", "0"))
 ADMIN_ID = int(os.getenv("ADMIN_TELEGRAM_ID", "0"))
