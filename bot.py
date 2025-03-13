fe662ca927df1fea2bc270f276c390c4058f36be
carlostoek <carlostoek@gmail.com>
2025-03-13 11:09:18 -0600

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,19 +1,25 @@
+import logging
 from aiogram import Bot, Dispatcher
+from aiogram.types import ParseMode
 from aiogram.utils import executor
-import os
-from database import init_db
 from handlers import register_handlers
+from database import init_db
 
-TOKEN = os.getenv("BOT_TOKEN")
+API_TOKEN = "TU_BOT_TOKEN"
 
-bot = Bot(token=TOKEN)
+# Configurar el logging para ver errores
+logging.basicConfig(level=logging.INFO)
+
+# Inicializar el bot y el dispatcher
+bot = Bot(token=API_TOKEN, parse_mode=ParseMode.HTML)
 dp = Dispatcher(bot)
 
-# Inicializar base de datos
+# Inicializar la base de datos
 init_db()
 
-# Registrar los handlers
+# Registrar los manejadores
 register_handlers(dp)
 
+# Ejecutar el bot
 if __name__ == "__main__":
     executor.start_polling(dp, skip_updates=True)
